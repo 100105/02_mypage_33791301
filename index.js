ec7191b3e77aadaa6be56ec8c1f8a17ae10364e1
Sara <sshah004@campus.goldsmiths.ac.uk>
2025-10-07 11:28:08 +0100

    initial version

---
+++ index.js
@@ -0,0 +1,20 @@
+var http = require("http");
+const port = 8000;
+
+http.createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Me</title>
+</head>
+<body>
+    <h1>My name is Sara.</h1>
+    <h2>I'm twenny</h2>
+    <p>I have 6 people in my family and they are all annoying.</p>
+</body>
+</html>`);
+}).listen(port, function() {
+    console.log(`Node server is running on port ${port}...`);
+});
